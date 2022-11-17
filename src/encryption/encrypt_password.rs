52a454c9d59b80405daf1f53fdb2361df6ab1972
sambernau <sambernau04@gmai.com>
2022-11-16 22:06:24 -0600

    File import examplessssssssssss

---
+++ src/encryption/encrypt_password.rs
@@ -0,0 +1,13 @@
+
+use crate::encryption::util::encrypt_util::times_two;
+
+
+pub fn final_number(number: i32) -> i32{
+    return times_two(number);
+}
+
+
+
+
+
+
---
+++ src/encryption/mod.rs
@@ -0,0 +1,2 @@
+pub mod encrypt_password;
+mod util;
