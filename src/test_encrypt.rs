46e7057a16abe8e530e1099d6b5c8f6d73727bae
Sam <sambernau04@gmail.com>
2022-11-22 16:05:04 -0600

    Finished encryption via user password

--- src/test_encrypt.rs
+++ src/test_encrypt.rs
@@ -22,42 +22,6 @@ use chacha20poly1305::{
 use crypto::aes_gcm::AesGcm;
 use crypto::aead::{AeadDecryptor};
 
-
-
-
-
-
-// pub fn test_encrypt_one() -> Result<(), chacha20poly1305::Error>  {
-//     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
-//     let cipher = ChaCha20Poly1305::new(&key);
-//     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
-//
-//     let mut buffer: Vec<u8> = Vec::new(); // Note: buffer needs 16-bytes overhead for auth tag
-//     buffer.extend_from_slice(b"plaintext message");
-//     println!("non-encrypted buffer = {buffer:?}");
-//
-// // Encrypt `buffer` in-place, replacing the plaintext contents with ciphertext
-//     cipher.encrypt_in_place(&nonce, b"", &mut buffer)?;
-//     println!("encrypted buffer = {:?}", buffer);
-// // `buffer` now contains the message ciphertext
-//     assert_ne!(&buffer, b"plaintext message");
-//
-// // Decrypt `buffer` in-place, replacing its ciphertext context with the original plaintext
-//     cipher.decrypt_in_place(&nonce, b"", &mut buffer)?;
-//     println!("back to non-encrypted buffer{:?}", buffer);
-//     assert_eq!(&buffer, b"plaintext message");
-//     Ok(())
-// }
-
-struct encrypt_return{
-    outtag:Vec<u8>,
-
-
-}
-
-
-
-
 fn hex_to_bytes(s: String) -> Vec<u8> {
     s.from_hex().unwrap()
 }
