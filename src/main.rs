4530794495d949118881f723bca122fb3823cba9
sambernau <sambernau04@gmai.com>
2022-12-06 08:43:51 -0600

    Added ui example

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use crate::user_functions::remove_account::remove_account;
 
 use crate::json::json_structs::{Account, AccountList};
 
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+
 
 
 fn main() {
@@ -17,13 +20,46 @@ fn main() {
     //
     // chacha20poly1305_decrypt_string("Elegantmuffin4421121@".to_string(),output,outtag);
     // //initialize_json().unwrap();
-    // add_account("Elegantmuffin4421121@".to_string(),
-    //             "Pornhub".to_string(),
-    //             "sbernau".to_string(),
-    //             "1121442@".to_string()).expect("TODO: panic message");
 
-    remove_account("Pornhub".to_string());
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
 
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+
+
+}
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Sam is the smartest person in the world *middle finger emoji*");
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("App")
+        .child(&button)
+        .show_menubar(true)
+        .build();
+
+    // Present window
+    window.present();
 }
 
 
